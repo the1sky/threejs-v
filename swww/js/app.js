c8c87fe6e603df15efa499ae9e731fd623334b76
dumingtan <dumingtan@baidu.com>
2015-09-15 15:00:05 +0800

    # init

---
+++ swww/js/app.js
@@ -0,0 +1,42 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+  });
+})
+  .config(function($ionicAppProvider, $stateProvider, $urlRouterProvider, $ionicConfigProvider){
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
+      //login
+      .state('test', {
+        url: '/test',
+        templateUrl: "templates/test.html",
+        controller: 'TestCtrl'
+      })
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/test');
+  })
---
+++ swww/js/controllers.js
@@ -0,0 +1,3 @@
+angular.module( 'starter.controllers' )
+    .controller( 'TestCtrl', function ($scope, LoginService,$ionicHistory, $ionicPopup, $ionicHistory, $state, CommonUtils,Global) {
+    } )
